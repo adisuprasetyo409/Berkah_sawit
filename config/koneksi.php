3ff3f8d35361dbf8ad70db3d66f6ce9ff5688e3c
adisuprasetyo409 <adisuprasetyo409@gmail.com>
2023-09-08 03:17:29 +0700

    test tess

--- config/koneksi.php
+++ config/koneksi.php
@@ -4,10 +4,10 @@
     $pass_db    = "";
     $nama_db    = "ls";
 
-    $koneksi    = mysqli_connect($host_db,$user_db,$pass_db,$nama_db) or die(mysqli_error($koneksi));;
-    $db = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
-    $conn = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
-    $connection = mysqli_connect($host_db, $user_db, $pass_db, $nama_db) or die (mysqli_connect_error());
+    //$koneksi    = mysqli_connect($host_db,$user_db,$pass_db,$nama_db) or die(mysqli_error($koneksi));;
+    //$db = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
+    //$conn = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
+    //$connection = mysqli_connect($host_db, $user_db, $pass_db, $nama_db) or die (mysqli_connect_error());
     // $koneksi = mysqli_connect($server, $user, $pass, $database) or die(mysqli_error($koneksi));
 
 
